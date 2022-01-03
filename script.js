8b15c36fc9bfc6ffdb40383b2df09bae9de89020
Blaz1q <57396007+Blaz1q@users.noreply.github.com>
2022-01-03 09:44:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,28 @@ function addd(vnumber){
     if(vnumber<10) return ("0"+vnumber);
     else return vnumber;
 }
+function losuj(min,max){
+  var pick = Math.floor(Math.random() * (max - min + 1)) + min;
+  var interval=20;
+  for(var i=0;i<150;i++){
+    interval+=(20*(i*i*i)*0.00001);
+    pick = Math.floor(Math.random() * (max - min + 1)) + min;
+    
+    setTimeout(() => {
+      pick = Math.floor(Math.random() * (max - min + 1)) + min;
+      document.getElementById('losowanie').innerHTML = "<h1 class='logo-1'>"+pick+"</h1>";
+      console.log(pick);
+  }, interval);
+}
+
+}
+
+function rolldice()
+{
+var ranNum = Math.floor( 1 + Math.random() * 6 );
+document.getElementById("dice").innerHTML = ranNum;
+
+}
 function iledni(nazwa,data){
     console.log(nazwa);
     console.log(data);
@@ -18,23 +40,22 @@ function iledni(nazwa,data){
     var sektext ="";
     sektext=addd(sek);
     mintext=addd(min);
+	htext=addd(h);
+	dtext=addd(dni);
 	if(sek%1==0)tick.play();
-    document.getElementById(nazwa).innerHTML = dni+"d "+h+"h "+mintext+"m "+sektext+"s ";
+	if(dni==0)
+	{
+		document.getElementById(nazwa).innerHTML = htext+"h "+mintext+"m "+sektext+"s ";
+		if(h==0){
+			document.getElementById(nazwa).innerHTML = mintext+"m "+sektext+"s ";
+				if(min==0) document.getElementById(nazwa).innerHTML = sektext+"s ";
+		}
+	}
+    else document.getElementById(nazwa).innerHTML = dni+"d "+htext+"h "+mintext+"m "+sektext+"s ";
     if (delta < 0) {
       clearInterval(odlicz);
-      document.getElementById(nazwa).innerHTML = "Termin minął";
+      document.getElementById(nazwa).innerHTML = "Matrix";
+      losuj(0,39);
     }
   }, 500);
-}
-var stop = false;
-function stopp(o){
-	if(o==0){
-	document.getElementById('stopprosze').innerHTML = "<input type='submit' value='Rozpocznij'  onclick='stopp(1),stoper()' />";
-	stop = true;
-	}
-	else{
-		document.getElementById('stopprosze').innerHTML = "<input type='submit' value='Zatrzymaj'  onclick='stopp(0),stoper()' />";
-		stop = false;
-	}
-	console.log(o);
 }
