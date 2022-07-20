051f6016af4a05416bdda5d796f582f16261a2d3
BlazOne <57396007+Blaz1q@users.noreply.github.com>
2022-07-21 00:08:43 +0200

    Add files via upload

--- loader.js
+++ loader.js
@@ -1,13 +1,13 @@
+var helper;
 function loader(){
     if (document.readyState === 'complete') {
         console.log('loaded');
+        helper = true;
         setTimeout(() => style_handler(), 800);
     }
 }
-
 function style_handler(){
     let elem = document.getElementsByClassName("loader wrap");
     elem[0].style.opacity = 0;
     setTimeout(() => elem[0].remove(), 1000);
-    
 }
